4840322d10b07e126f2bf1d3555be6eb298368c8
Michelle Carino <macarino@hawaii.edu>
2019-03-08 23:23:31 -1000

    remove search form from not found page

--- loop-courses.php
+++ loop-courses.php
@@ -18,7 +18,6 @@
     <h1 class="entry-title"><?php _e( 'Not Found', 'manoa2018' ); ?></h1>
     <div class="entry-content">
       <p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'manoa2018' ); ?></p>
-      <?php get_search_form(); ?>
     </div><!-- .entry-content -->
   </div><!-- #post-0 -->
 <?php endif; ?>
