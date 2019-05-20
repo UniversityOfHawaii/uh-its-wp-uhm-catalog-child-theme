35e5e7f25f26d559d48948264c265cdf60e72015
Michelle Carino <macarino@hawaii.edu>
2019-05-20 11:04:48 -1000

    add margins in search results

--- search.php
+++ search.php
@@ -8,8 +8,7 @@ get_header();
 
   <main id="main_area" class="full-width">
     <div id="main_content">
-      <div id="container">
-      <div id="content" role="main">
+      <div class="container" id="content" role="main">
 
         <?php if ( have_posts() ) : ?>
           <?php get_template_part( 'loop', 'search' ); ?>
@@ -22,7 +21,6 @@ get_header();
           </div><!-- #post-0 -->
         <?php endif; ?>
 
-        </div><!-- #content -->
       </div><!-- #container -->
 
 <?php get_footer(); ?>
