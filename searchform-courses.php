dd32783caa090cddc2306deacf4cffc78b3c0149
Michelle Carino <macarino@hawaii.edu>
2019-05-20 16:52:49 -1000

    change id of course search container

--- searchform-courses.php
+++ searchform-courses.php
@@ -3,7 +3,7 @@
  * Template for displaying courses search form
  */
 ?>
-<form role="search" method="get" class="search-form" id="courses-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="search-form" id="courses-search-container" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="hidden assistive-text screen-reader-text" for="courses-search"><?php echo _e( 'Search courses:', 'label' ); ?>
 	</label>
 	<input type="search" class="search-field" placeholder="<?php echo esc_attr_e( 'Enter search terms', 'placeholder' ); ?>" value="<?php echo get_search_query(); ?>" name="s" id="courses-search" />
