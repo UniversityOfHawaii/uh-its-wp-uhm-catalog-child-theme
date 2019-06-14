e93d9963ac27a7e175cf70455da2e5e0dc6a3e20
Michelle Carino <macarino@hawaii.edu>
2019-06-14 10:42:17 -1000

    remove hard-coded links and add template for uhm-header

--- header.php
+++ header.php
@@ -49,45 +49,12 @@ if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,700" rel="stylesheet">
 <!-- load font awesome icons -->
 <script src="https://use.fontawesome.com/bfcbe1540c.js"></script>
-<?php if( is_page_template('page-onecolumn.php') || is_front_page() ) : ?>
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.css" />
-<?php endif; ?>
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <header id="top">
-  <a href="#main_area" id="skip2main">Skip to Main Content</a>
-  <div id="header_top">
-    <div id="header_top_content">
-      <ul id="header_mainmenu">
-        <li class="show-on-mobile"><a href="https://manoa.hawaii.edu/">UHM Home</a></li>
-        <li class="show-on-mobile"><a href="https://manoa.hawaii.edu/a-z/">A-Z Index</a></li>
-        <li class="show-on-mobile"><a href="https://manoa.hawaii.edu/directory/">Directory</a></li>
-        <li><a href="https://manoa.hawaii.edu/students/">Students</a></li>
-        <li><a href="https://manoa.hawaii.edu/faculty-staff/">Faculty and Staff</a></li>
-        <li><a href="https://manoa.hawaii.edu/admissions/parents.html">Parents</a></li>
-        <li><a href="https://uhalumni.org/manoa/">Alumni</a></li>
-        <li class="show-on-mobile"><a href="https://myuh.hawaii.edu/">MyUH</a></li>
-      </ul>
-      <div id="header_smrow">
-        <a href="https://twitter.com/UHManoa">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-twitter.png" alt="twitter" class="header_smicon" />
-        </a> &nbsp;
-        <a href="https://www.facebook.com/uhmanoa">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-facebook.png" alt="facebook" class="header_smicon" />
-        </a> &nbsp;
-        <a href="https://instagram.com/uhmanoanews">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-instagram.png" alt="instagram" class="header_smicon" />
-        </a> &nbsp;
-        <a href="http://www.flickr.com/photos/uhmanoa">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-flickr.png" alt="flickr" class="header_smicon" />
-        </a> &nbsp;
-        <a href="http://www.youtube.com/user/UniversityofHawaii">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-youtube.png" alt="youtube" class="header_smicon" /></a>
-      </div>
-    </div>
-  </div>
+  <?php get_template_part('uhm-header'); ?>
   <div id="header_mid">
     <div class="container">
       <a class="site-name-description" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
